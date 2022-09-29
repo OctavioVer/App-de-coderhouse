cfdafba3217da96edaed8cfa0bdc087d872bd99f
Octavio <90575217+OctavioVer@users.noreply.github.com>
2022-09-28 22:51:40 -0300

    Entrega de errores

--- src/componentes/ItemDetailContainer.jsx
+++ src/componentes/ItemDetailContainer.jsx
@@ -1,89 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 import ItemDetail from "./ItemDetail";
 import { useParams } from "react-router-dom";
-
-const detalle = [
-  {
-    id: 1,
-    categoria: "Indumentaria ",
-    titulo: "Buzo Fila",
-    descripcion:
-      "Some quick example text to build on the card title and make up the bulk of the cards content.",
-    price: "$4200 Pesos",
-    foto: "https://ferreira.vteximg.com.br/arquivos/ids/383149-540-540/fi_f11l5060126577.jpg?v=637693091478770000",
-    stock: 5,
-  },
-  {
-    id: 2,
-    categoria: "Indumentaria ",
-    titulo: "Buzo Oversize",
-    descripcion:
-      "Some quick example text to build on the card title and make up the bulk of the cards content.",
-    price: "$4200 Pesos",
-    foto: "https://ferreira.vteximg.com.br/arquivos/ids/393736-540-540/ch_ICHHS9406018.jpg?v=637847767284500000",
-    stock: 5,
-  },
-  {
-    id: 3,
-    categoria: "Indumentaria ",
-    titulo: "Buzo Tommy Hilfiger",
-    descripcion:
-      "Some quick example text to build on the card title and make up the bulk of the cards content.",
-    price: "$4200 Pesos",
-    foto: "https://tommyargentina.vteximg.com.br/arquivos/ids/204391-500-667/MW0MW24594_DW5_1.jpg?v=637927178658100000",
-    stock: 5,
-  },
-  {
-    id: 4,
-    categoria: "Indumentaria ",
-    titulo: "Buzo Adidas",
-    descripcion:
-      "Some quick example text to build on the card title and make up the bulk of the cards content.",
-    price: "$4200 Pesos",
-    foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaKyjl2uHOvqlR7TNGLfylUNSbSemMesnUFQ&usqp=CAU",
-    stock: 5,
-  },
-  {
-    id: 5,
-    categoria: "Indumentaria ",
-    titulo: "Buzo Taverniti",
-    descripcion:
-      "Some quick example text to build on the card title and make up the bulk of the cards content.",
-    price: "$4200 Pesos",
-    foto: "https://taverniti.vteximg.com.br/arquivos/ids/247047-345-345/04711_649X1.jpg?v=637968633629100000",
-    stock: 5,
-  },
-  {
-    id: 6,
-    categoria: "Indumentaria ",
-    titulo: "Buzo Koaj Verde",
-    descripcion:
-      "Some quick example text to build on the card title and make up the bulk of the cards content.",
-    price: "$4200 Pesos",
-    foto: "https://cdn.koaj.co/116208-home_mobile_default/buzo-con-capota-estampado.jpg",
-    stock: 5,
-  },
-  {
-    id: 7,
-    categoria: "Indumentaria ",
-    titulo: "Buzo Universitario",
-    descripcion:
-      "Some quick example text to build on the card title and make up the bulk of the cards content.",
-    price: "$4200 Pesos",
-    foto: "https://cdn.koaj.co/104517-home_mobile_default/buzo-con-capota-estampado.jpg",
-    stock: 5,
-  },
-  {
-    id: 8,
-    categoria: "Indumentaria ",
-    titulo: "Buzo Rosa Cropped",
-    descripcion:
-      "Some quick example text to build on the card title and make up the bulk of the cards content.",
-    price: "$4200 Pesos",
-    foto: "https://cdn.koaj.co/c/371-category_default/cropped.jpg",
-    stock: 5,
-  },
-];
 
 const ItemDetailContainer = () => {
   const [data, setData] = useState({});
@@ -91,15 +9,11 @@ const ItemDetailContainer = () => {
   const { itemId } = useParams();
 
   useEffect(() => {
-    const getData = new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(detalle);
-      }, 2000);
+    const db = getFirestore();
+    const response = doc(db, "productos", "0pisdeIqaf9ncwMUuN1d");
+    getDoc(response).then((snapshot) => {
+      console.log(snapshot);
     });
-
-    getData.then((res) =>
-      setData(res.find((detalle) => detalle.id === parseInt(itemId)))
-    );
   }, [itemId]);
 
   return <ItemDetail data={data} />;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD7ct8of6GF0Z_jyWdJDoe_AOWUziUInbo",
+  authDomain: "appcoderhousevercellone.firebaseapp.com",
+  projectId: "appcoderhousevercellone",
+  storageBucket: "appcoderhousevercellone.appspot.com",
+  messagingSenderId: "272256987306",
+  appId: "1:272256987306:web:3f71af11221ead7350a0d3",
+  measurementId: "G-YE7NFD7X4K"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
